25320bf4b6a3d9950c4f970533971a188e137041
Tim Hoolihan <tim@hoolihan.net>
2016-05-24 17:36:37 -0400

    adding neural net

--- nn.R
+++ nn.R
@@ -6,10 +6,10 @@ library(foreach)
 library(caret)
 library(neuralnet)
 
-test_run = TRUE
+test_run = FALSE
 
 # read
-column.types <- c('integer', 'factor', 'factor', 'character', 'factor', 
+column.types <- c('integer', 'integer', 'integer', 'character', 'factor', 
                   'numeric', 'integer', 'integer', 'character', 'numeric', 
                   'character', 'character')
 train.raw <- read.csv("data/train.csv", 
@@ -23,17 +23,16 @@ test.raw <- read.csv("data/test.csv",
 
 # prepare
 titanic.prepare <- function(df, median_age, median_fare) {
-  df <- mutate(df,
-               Family = as.integer(SibSp + Parch + 1),
-               Age = ifelse(is.na(Age), median_age, Age),
-               Fare = ifelse(is.na(Fare), median_fare, Fare),
-               Embarked = ifelse(is.na(Embarked), 'S', Embarked)) %>%
-    dplyr::select(-Name, -Fare, -Cabin, -Ticket)
-  df$Pclass <- revalue(df$Pclass, c("1" = "First", "2" = "Second", "3" = "Third"))
-  if('Survived' %in% colnames(df)) { 
-    df$Survived = revalue(df$Survived, c("0" = "Perished", "1" = "Survived"))
-  }
-  df
+  mutate(df,
+         FirstClass = ifelse(Pclass == 1, 1, 0),
+         SecondClass = ifelse(Pclass == 2, 1, 0),
+         ThirdClass = ifelse(Pclass == 3, 1, 0),
+         Sex = ifelse(Sex == "male", 0, 1),
+         Family = as.integer(SibSp + Parch + 1),
+         Age = ifelse(is.na(Age), median_age, Age),
+         Fare = ifelse(is.na(Fare), median_fare, Fare),
+         Embarked = ifelse(is.na(Embarked), 'S', Embarked)) %>%
+    dplyr::select(-Name, -Fare, -Cabin, -Ticket, -Pclass)
 }
 
 # get median values for fields that have NAs
@@ -54,29 +53,31 @@ if(test_run) { # split the training set
 }
 
 # train
-nn <- neuralnet(Survived ~ ., data = data.train, hidden = 4, lifesign = "minimal",
-                linear.output = FALSE, threshold = 0.1)
+nn <- neuralnet(Survived ~ Sex + Age + FirstClass + SecondClass + ThirdClass + Family, 
+                data = data.train, 
+                hidden = c(6, 4, 2), 
+                lifesign = "minimal",
+                linear.output = FALSE, 
+                threshold = 0.1, 
+                rep = 1)
 
 # predict
-data.test$nn_output <- round(compute(nn, data.test))
+test <- dplyr::select(data.test, Sex, Age, FirstClass, SecondClass, ThirdClass, Family)
+results <- compute(nn, test)
+data.test$nn_output <- as.integer(round(results$net.result))
 
+plot(nn, rep = "best")
 if(test_run) {
-  plot(nn, rep = "best")
-  confusionMatrix(data.test$nn_output, data.test$Survived)
+  print(confusionMatrix(data.test$nn_output, data.test$Survived))
 } else {
   ts = format(Sys.time(), "%Y.%m.%d.%H.%M.%S")
-  write_results <- function(df, name, output_col, subdir = ts) {
-    df2 <- mutate(df, Survived = revalue(output_col, c('Perished' = 0, 'Survived' = 1))) %>%
-      dplyr::select(PassengerId, Survived)
-    dir = paste('output/', subdir, sep = "")
-    if(!dir.exists(dir)) {
-      dir.create(dir)
-    }
-    fname <- paste(dir, '/', name, sep = "")
-    write.csv(df2, file = fname, row.names = FALSE, quote = FALSE)
-    print(paste('wrote', fname))
-  }
-  write_results(data.test, 'nn.csv', data.test$nn_output)
+  df <- mutate(data.test, Survived = nn_output)
+  df <-  dplyr::select(df, PassengerId, Survived)
+  dir = paste('output/', ts, sep = "")
+  if(!dir.exists(dir)){ dir.create(dir) }
+  fname <- paste(dir, '/nn.csv', sep = '')
+  write.csv(df, file = fname, row.names = FALSE, quote = FALSE)
+  print(paste('wrote', fname))
 }
 
 
