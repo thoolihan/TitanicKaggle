5c67c776d12b82bb6d55818657f638becbb66d9b
Tim Hoolihan <tim@hoolihan.net>
2016-05-24 13:25:42 -0400

    Added bayesglm

--- train_caret.R
+++ train_caret.R
@@ -7,27 +7,29 @@ library(caret)
 library(e1071)
 library(pROC)
 
-test_run = TRUE
+test_run = FALSE
 
 # read
 column.types <- c('integer', 'factor', 'factor', 'character', 'factor', 
-                  'numeric', 'integer', 'integer', 'character', 'numeric', 'character', 'character')
+                  'numeric', 'integer', 'integer', 'character', 'numeric', 
+                  'character', 'character')
 train.raw <- read.csv("data/train.csv", 
                       colClasses = column.types,
                       na.strings = c("NA", ""),
                       header = TRUE)
-test.raw <- read.csv("data/test.csv", header = TRUE, colClasses = column.types[-2])
+test.raw <- read.csv("data/test.csv", 
+                     na.strings = c("NA", ""), 
+                     header = TRUE, 
+                     colClasses = column.types[-2])
 
 # prepare
 titanic.prepare <- function(df, median_age, median_fare) {
   df <- mutate(df,
-         SibSp = SibSp,
-         Parch = Parch,
          Family = as.integer(SibSp + Parch + 1),
          Age = ifelse(is.na(Age), median_age, Age),
          Fare = ifelse(is.na(Fare), median_fare, Fare),
          Embarked = ifelse(is.na(Embarked), 'S', Embarked)) %>%
-    select(-Name, -Fare, -Cabin, -Ticket)
+    dplyr::select(-Name, -Fare, -Cabin, -Ticket)
   df$Pclass <- revalue(df$Pclass, c("1" = "First", "2" = "Second", "3" = "Third"))
   if('Survived' %in% colnames(df)) { 
     df$Survived = revalue(df$Survived, c("0" = "Perished", "1" = "Survived"))
@@ -43,7 +45,7 @@ data.train <- titanic.prepare(train.raw, m_age, m_fare)
 
 # split data
 if(test_run) { # split the training set
-  set.seed(4)
+  set.seed(100)
   train.rows <- createDataPartition(data.train$Survived, p = 0.8, list = FALSE)
   data.test <- data.train[-(train.rows),]
   data.train <- data.train[train.rows,]
@@ -58,50 +60,62 @@ cv.ctrl <- trainControl(method = "repeatedcv",
                         summaryFunction = twoClassSummary,
                         classProbs = TRUE)
 
-pls_tg <- data.frame(.ncomp = 1:23)
-pls_model <- train(Survived ~ Sex * Age * Pclass * I(SibSp + Parch), 
-             data = data.train, 
-             method = "pls",
-             metric = "ROC",
-             tuneGrid = pls_tg,
-             preProcess = c("center", "scale"),
-             trControl = cv.ctrl)
+build_model <- function(form, learn_method, tune_grid) {
+  train(form, 
+        data = data.train, 
+        method = learn_method,
+        metric = "ROC",
+        tuneGrid = tune_grid,
+        preProcess = c("center", "scale"),
+        trControl = cv.ctrl)
+}
+
+pls_tg <- data.frame(.ncomp = 3:10)
+pls_model <- build_model(Survived ~ .,
+                         learn_method = "pls", 
+                         tune_grid = pls_tg)
 
 rf_tg <- data.frame(.mtry = 2:8)
-rf_model <- train(Survived ~ Sex * Age * Pclass * I(SibSp + Parch), 
-                   data = data.train, 
-                   method = "rf",
-                   metric = "ROC",
-                   tuneGrid = rf_tg,
-                   preProcess = c("center", "scale"),
-                   trControl = cv.ctrl)
+rf_model <- build_model(Survived ~ .,
+                        learn_method = "parRF", 
+                        tune_grid = rf_tg)
 
+ba_model <- train(Survived ~ ., 
+                  data = data.train, 
+                  method = "bayesglm",
+                  metric = "ROC",
+                  preProcess = c("center", "scale"),
+                  trControl = cv.ctrl)
 
 # predict
 data.test$pls_output <- predict(pls_model, data.test)
 data.test$rf_output <- predict(rf_model, data.test)
+data.test$ba_output <- predict(ba_model, data.test)
 
 if(test_run) {
   print(plot(pls_model))
   print(plot(rf_model))
   resamp <- resamples(list(pls = pls_model,
-                           rf = rf_model))
+                           rf = rf_model,
+                           ba = ba_model))
   print(summary(resamp))
+  print(xyplot(resamp, what = "BlandAltman"))
 } else {
   ts = format(Sys.time(), "%Y.%m.%d.%H.%M.%S")
   write_results <- function(df, name, output_col, subdir = ts) {
-    df <- mutate(df, Survived = revalue(output_col, c('Perished' = 0, 'Survived' = 1))) %>%
-      select(PassengerId, Survived)
+    df2 <- mutate(df, Survived = revalue(output_col, c('Perished' = 0, 'Survived' = 1))) %>%
+      dplyr::select(PassengerId, Survived)
     dir = paste('output/', subdir, sep = "")
     if(!dir.exists(dir)) {
       dir.create(dir)
     }
     fname <- paste(dir, '/', name, sep = "")
-    write.csv(df, file = fname, row.names = FALSE, quote = FALSE)
+    write.csv(df2, file = fname, row.names = FALSE, quote = FALSE)
     print(paste('wrote', fname))
   }
   write_results(data.test, 'pls.csv', data.test$pls_output)
   write_results(data.test, 'rf.csv', data.test$rf_output)
+  write_results(data.test, 'ba.csv', data.test$ba_output)
 }
 
 
